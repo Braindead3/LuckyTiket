4b2dc12930719de9b311f966ef9c648b59096829
Vlad <malosay21@gmail.com>
2020-12-12 09:24:34 +0300

    commit

--- LuckyTiket/Program.cs
+++ LuckyTiket/Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace LuckyTiket
 {
     class Program
     {
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
         static void Main(string[] args)
         {
+            Random random = new Random();
+            List<Tiket> tikets = new List<Tiket>();
+            for (int i = 0; i < 100000; i++)
+            {
+                tikets.Add(new Tiket());
+                tikets[i].FillTiket();
+                tikets[i].LuckyChiNe();
+            }
 
+            int counter = tikets.Where(x => x.lucky == true).Count();
+
+            if (counter > 0)
+            {
+                for (int i = 0; i < counter; i++)
+                {
+                    Console.WriteLine("+");
+                }
+            }
+            Console.ReadLine();
         }
     }
+
 }
+
---
+++ LuckyTiket/Tiket.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LuckyTiket
+{
+    class Tiket
+    {
+        int[] leftSide = new int[3];
+        int[] rightSide = new int[3];
+        public bool lucky;
+
+        public void FillTiket()
+        {
+            string str="";
+
+            for (int i = 0; i < leftSide.Length; i++)
+            {
+                str = "";
+                for (int j = 0; j < 3; j++)
+                {
+                    var random = RandomProvider.GetThreadRandom();
+                    str += Convert.ToString(random.Next(1,10));
+                }
+                leftSide[i] = Convert.ToInt32(str);
+            }
+
+            for (int i = 0; i < rightSide.Length; i++)
+            {
+                str = "";
+                for (int j = 0; j < 3; j++)
+                {
+                    var random = RandomProvider.GetThreadRandom();
+                    str += Convert.ToString(random.Next(1,10));
+                }
+                rightSide[i] = Convert.ToInt32(str);
+            }
+        }
+
+        public void LuckyChiNe()
+        {
+            int leftSum = 0, RightSum = 0;
+            for (int i = 0; i < leftSide.GetLength(0); i++)
+            {
+                    leftSum += leftSide[i];
+            }
+
+            for (int i = 0; i < rightSide.GetLength(0); i++)
+            {
+                    RightSum += rightSide[i];
+            }
+
+            if (leftSum == RightSum)
+            {
+                lucky = true;
+            }
+            else
+            {
+                lucky = false;
+            }
+        }
+
+        public override string ToString()
+        {
+            for (int i = 0; i < leftSide.Length; i++)
+            {
+                Console.WriteLine($"{leftSide[i]}{rightSide[i]}");
+            }
+            return "";
+        }
+    }
+}
